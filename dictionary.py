6e07d691407b06d976e725e0c37bf2029b72a7c8
Abdulrahman <94384418+abdulrahman0842@users.noreply.github.com>
2022-08-16 22:17:52 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,10 @@ dictionary2={'GFG':1,'Microsoft':2,'Youtub':3}
 dictionary1.update(dictionary2);
 for key,values in dictionary1.items():
     print(key,values)
+    
+#2
+temp=dict()
+temp['key1']={'key1':44,'key2':566}
+temp['key2']=[1,2,3,4]
+for (key,values) in temp.items():
+    print(values,end="")
